4e6264d337334d15c3271ec289fcc6cecf850910
admin <admin@126.com>
2019-02-28 20:31:05 +0800

    修改GitTest内容

--- git-day01-second/src/main/java/cn/itheima/git/GitTest.java
+++ git-day01-second/src/main/java/cn/itheima/git/GitTest.java
@@ -3,5 +3,7 @@ package cn.itheima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,git.I'm coming.");
+
+        System.out.println("老夫聊发少年狂，左牵黄，右擎苍。");
     }
 }
